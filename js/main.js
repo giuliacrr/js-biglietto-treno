278ac4f8fac546837f0eeb4684d97b805df5b93a
giuliacrr <giulia.corrado02@gmail.com>
2023-06-30 18:31:41 +0200

    Added date

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
-//const fullName =prompt("What's your full name?");
+const fullName =prompt("What's your full name?");
 const userAge = parseInt(prompt("How old are you?"));
 const userKilometers = parseInt(prompt("How many kilometers you want to travel?"));
 const price = userKilometers * 0.21;
+const date = new Date();
 
+// Date
+const currentDay = date.getDate()
+const currentMonth = date.getUTCMonth() + 1 ;
+const currentYear = date.getFullYear();
+const currentDate = `${currentDay}/${currentMonth}/${currentYear}`
+document.getElementById("currentDate").innerHTML = `${currentDate}`
 
-
-//document.getElementById("fullName").innerHTML = `${fullName}`
+//Informations
+document.getElementById("fullName").innerHTML = `${fullName}`
 document.getElementById("userAge").innerHTML = `${userAge}`
 document.getElementById("userKilometers").innerHTML = `${userKilometers}`
 
 
 
 if (userAge < 18) {
-  const coupon = ((price * 20) / 100);
-    console.log(price - coupon);
+    const coupon = ((price * 20) / 100);
+    document.getElementById("price").innerHTML = (price - coupon).toFixed(2) + "€";
 
 } else if (userAge >= 65) {
     const coupon = ((price * 40) / 100);
-    console.log(price - coupon);
-    
+    document.getElementById("price").innerHTML = (price - coupon).toFixed(2) + "€";
+
 } else {
     console.log(price);
+    document.getElementById("price").innerHTML = (price).toFixed(2) + "€";
 }
